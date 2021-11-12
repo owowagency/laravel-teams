3cc6b3344e49a94e908ebd1c0345ec7b86ffbac7
Dees Oomens <deesoomens@gmail.com>
2021-11-12 11:09:58 +0100

    🐛 Fixed linteR

--- tests/Unit/Models/Concerns/InteractsWithInvitationsTest.php
+++ tests/Unit/Models/Concerns/InteractsWithInvitationsTest.php
@@ -14,8 +14,8 @@ use OwowAgency\Teams\Exceptions\InvitationAlreadyAccepted;
 use OwowAgency\Teams\Exceptions\InvitationAlreadyDeclined;
 use OwowAgency\Teams\Models\Invitation;
 use OwowAgency\Teams\Models\Team;
-use OwowAgency\Teams\Tests\Support\Models\User;
 use OwowAgency\Teams\TeamAssertions;
+use OwowAgency\Teams\Tests\Support\Models\User;
 use OwowAgency\Teams\Tests\TestCase;
 
 class InteractsWithInvitationsTest extends TestCase
