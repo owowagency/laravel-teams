a8bbb23d9c65666267b9b7b7764d2f4d92909d14
Dees Oomens <deesoomens@gmail.com>
2021-12-22 14:43:20 +0100

    🔀 Switch around enum value to make sure callable doesn't call Request facade

--- src/Enums/InvitationType.php
+++ src/Enums/InvitationType.php
@@ -7,12 +7,12 @@ use BenSampo\Enum\Enum;
 final class InvitationType extends Enum
 {
     /**
-     * The user has requested to join the team.
-     */
-    const REQUEST = 0;
-
-    /**
      * The user has been invited to join the team.
      */
-    const INVITATION = 1;
+    const INVITATION = 0;
+
+    /**
+     * The user has requested to join the team.
+     */
+    const REQUEST = 1;
 }
